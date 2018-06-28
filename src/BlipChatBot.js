6c0229b61df824695c003bcde3cb9ed9cc50be7a
JulioCR <rodrigues327@gmail.com>
2018-06-28 20:53:53 -0300

    added typing

--- src/BlipChatBot.js
+++ src/BlipChatBot.js
@@ -1,10 +1,10 @@
 var BlipSdk = require('blip-sdk');
 let Lime = require('lime-js');
 var WebSocketTransport = require('lime-transport-websocket')
+
 module.exports = function (Botkit, config) {
 
     var controller = Botkit.core(config);
-
 
     controller.defineBot(function (botkit, config) {
 
@@ -14,7 +14,6 @@ module.exports = function (Botkit, config) {
             utterances: botkit.utterances,
         }
 
-
         // Create a client instance passing the identifier and accessKey of your chatbot 
         let client = new BlipSdk.ClientBuilder()
             .withIdentifier(config.identifier)
@@ -22,11 +21,8 @@ module.exports = function (Botkit, config) {
             .withTransportFactory(() => new WebSocketTransport())
             .build();
 
-
-        client.connect();
         // connect to the MessagingHub server
-
-
+        client.connect();
 
         bot.send = async function (message, cb) {
 
@@ -43,42 +39,31 @@ module.exports = function (Botkit, config) {
                 return;
             }
 
-
-            // Copy message minus user & channel fields
-            var bf_message = {};
-            for (var key in message) {
-                switch (key) {
-                    case 'user':
-                    case 'channel':
-                        // ignore
-                        break;
-                    default:
-                        bf_message[key] = message[key];
-                        break;
-                }
-            }
-            if (!bf_message.type) {
-                bf_message.type = 'message';
+            if (!message.type) {
+                message.type = 'message';
             }
 
-
+            await controller.sendTyping(bot, message.to, message.typingTime || 1000)
 
             // Send message through connector
-            client.sendMessage([bf_message])
+            client.sendMessage(message)
             done();
-
         }
 
         // this function takes an incoming message (from a user) and an outgoing message (reply from bot)
         // and ensures that the reply has the appropriate fields to appear as a reply
         bot.reply = async function (src, resp, cb) {
 
-            let header = {
+            var msg = {
                 id: Lime.Guid(),
                 to: src.from
-            }
+            };
 
-            let msg = Object.assign(header, resp);
+            if (typeof (resp) == 'string') {
+                msg.text = resp;
+            } else {
+                msg = Object.assign(msg, resp);
+            }
 
             if (!msg.content) {
                 msg.content = msg.text;
@@ -91,8 +76,8 @@ module.exports = function (Botkit, config) {
             bot.say(msg, cb);
         }
 
-
         bot.findConversation = function (message, cb) {
+
             botkit.debug('CUSTOM FIND CONVO', message.user, message.channel);
             for (var t = 0; t < botkit.tasks.length; t++) {
                 for (var c = 0; c < botkit.tasks[t].convos.length; c++) {
@@ -108,15 +93,11 @@ module.exports = function (Botkit, config) {
                     }
                 }
             }
-
             cb();
         };
         bot.connect = client;
         return bot;
     })
-
-
-
 
     // provide one or more normalize middleware functions that take a raw incoming message
     // and ensure that the key botkit fields are present -- user, channel, text, and type
@@ -140,6 +121,7 @@ module.exports = function (Botkit, config) {
     // the necessary format required by the platform API
     // at a minimum, copy all fields from `message` to `platform_message`
     controller.middleware.format.use(function (bot, message, platform_message, next) {
+
         for (var k in message) {
             platform_message[k] = message[k]
         }
@@ -149,7 +131,6 @@ module.exports = function (Botkit, config) {
     // provide a way to receive messages - normally by handling an incoming webhook as below!
     controller.createWebhookEndpoints = (bot, cb) => {
 
-
         bot.connect.addMessageReceiver(() => true, (message) => {
             controller.ingest(bot, message, null);
         })
@@ -158,34 +139,31 @@ module.exports = function (Botkit, config) {
             cb();
         }
 
+        controller.startTicking();
+
         return controller;
     };
 
-    sleep = (ms) =>{
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+    controller.sendTyping = async (bot, to, time) => {
 
-
-    controller.sendTyping = async (bot, message, time) => {
-
-        bot.reply(message, {
+        var message = {
+            id: Lime.Guid(),
+            to: to,
             type: "application/vnd.lime.chatstate+json",
-            content: {
-                "state": "composing"
-            }
-        })
+            content: { "state": "composing" }
+        };
+
+        bot.connect.sendMessage(message)
 
         await sleep(time);
 
-        bot.reply(message, {
-            type: "application/vnd.lime.chatstate+json",
-            content: {
-                "state": "starting"
-            }
-        })
+        message.content = { "state": "starting" }
+        bot.connect.sendMessage(message)
     }
 
+    sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     return controller;
-
 }
